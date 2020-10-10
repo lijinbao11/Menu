294ea62d22c8ab4336d88cf56e5aef440b19a10d
lijinbao11 <l18994791837@163.com>
2020-10-10 17:33:25 +0800

    菜单显示, 有bug

---
+++ SystemMenu/Controllers/ManagerController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SystemMenu.Model;
+using SystemMenu.Model.Entities.Permission;
+
+namespace SystemMenu.Controllers
+{
+    public class ManagerController : Controller
+    {
+        private readonly SystemMenuDBContext _dbContext;
+
+        public ManagerController(SystemMenuDBContext dbContext)
+        {
+
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetManagers()
+        {
+            var data = _dbContext.managers.ToList();
+            return new JsonResult(new { code = 0, msg = "", count = data.Count, data });
+
+        }
+    }
+}
---
+++ SystemMenu/Controllers/MenuController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SystemMenu.Model;
+
+namespace SystemMenu.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly SystemMenuDBContext _dbContext;
+
+        public MenuController(SystemMenuDBContext dbContext)
+        {
+
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetMenuList() 
+        {
+            var menu = _dbContext.systemMenus.ToList();
+            return new JsonResult(new { code = 0, msg = "", count = menu.Count, menu });
+        }
+    }
+}
