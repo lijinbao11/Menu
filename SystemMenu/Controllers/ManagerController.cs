d0caa5dab6a226ede9da7af6bd402f736540f1fe
lijinbao11 <l18994791837@163.com>
2020-10-13 17:46:44 +0800

    添加用户,和删除用户信息

--- SystemMenu/Controllers/ManagerController.cs
+++ SystemMenu/Controllers/ManagerController.cs
@@ -27,7 +27,58 @@ namespace SystemMenu.Controllers
         {
             var data = _dbContext.managers.ToList();
             return new JsonResult(new { code = 0, msg = "", count = data.Count, data });
+        }
 
+
+        #region 添加
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 添加
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public JsonResult AddManagers(Manager manager)
+        {
+            if (ModelState.IsValid)
+            {
+                manager.CreateTime = Convert.ToDateTime(DateTime.Now.ToString());
+                manager.IsEnable = true;
+                _dbContext.Add(manager);
+                if ( _dbContext.SaveChanges() > 0)
+                    return Json(new { success = true, msg = "新增成功" });
+                return Json(new { success = false, msg = "提交失败" });
+            }
+            return Json(new { success = false, msg = "提交数据有误，请重新提交" });
+        }
+        #endregion
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var manager = await _dbContext.managers.FindAsync(id);
+            if (manager == null)
+            {
+                return Json(new { sussess = false, msg = "数据不存在或已经删除" });
+            }
+
+            _dbContext.managers.Remove(manager);
+
+                if ( _dbContext.SaveChanges() > 0)
+                {
+                    return Json(new { success = true, msg = "删除成功" });
+                }
+
+            return Json(new { success = false, msg = "提交数据有误，请重新提交" });
+        }
+
+
+        public IActionResult Edit()
+        {
+            return View();
         }
     }
 }
